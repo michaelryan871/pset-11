4c1d27212face384781ee353a68bb577100ab0bb
Michael Ryan <mike.ryan871@gmail.com>
2020-03-22 18:48:21 -0400

    Exercise 6 PT.1

--- src/Exercises.java
+++ src/Exercises.java
@@ -100,6 +100,23 @@ public class Exercises {
 }
 
   public ArrayList<String> bubble(ArrayList<String> list, boolean ascending) {
+    if (list == null || list.size() == 0){
+      return null;
+    }
+
+    int n = list.size();
+    if (ascending) {
+      for (int i = 0; i < (n-1); i++){
+        for (int j = 0; j < n-i-1; j++){
+          if (list.get(j).compareTo(list.get(j-1)) > 0){
+            String temp = list.get(j);
+            list.set(j, list.get(j-1));
+            list.set(j - 1, temp);
+          }
+        }
+      }
+    }
+
     return null;
   }
 
