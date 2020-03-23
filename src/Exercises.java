9bbf5b4ef56db15e27de8a7c68c2d4d414a78e78
Michael Ryan <mike.ryan871@gmail.com>
2020-03-22 23:14:16 -0400

    Exercise 12 Part 2.

--- src/Exercises.java
+++ src/Exercises.java
@@ -330,3 +330,33 @@ public class Exercises {
 
         return list;
       }
+
+     private void combineHalves(ArrayList < Integer > left, ArrayList < Integer > right, ArrayList < Integer > complete) {
+     int leftIndex = 0;
+     int rightIndex = 0;
+     int completeArrayIndex = 0;
+     while (leftIndex < left.size() && rightIndex < right.size()) {
+         if ((left.get(leftIndex).compareTo(right.get(rightIndex))) < 0) {
+             complete.set(completeArrayIndex, left.get(leftIndex));
+             leftIndex++;
+         } else {
+             complete.set(completeArrayIndex, right.get(rightIndex));
+             rightIndex++;
+         }
+         completeArrayIndex++;
+     }
+     ArrayList < Integer > rest;
+     int restIndex;
+     if (leftIndex >= left.size()) {
+         rest = right;
+         restIndex = rightIndex;
+     } else {
+         rest = left;
+         restIndex = leftIndex;
+     }
+
+     for (int i = restIndex; i < rest.size(); i++) {
+         complete.set(completeArrayIndex, rest.get(i));
+         completeArrayIndex++;
+     }
+ }
